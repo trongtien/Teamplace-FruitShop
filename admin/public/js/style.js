d57480d7158f7ce8cbd790909b1768fed6d73a70
trongtien <phanletrongtien@gmail.com>
2020-02-03 00:04:14 +0700

    success menu;

--- admin/public/js/style.js
+++ admin/public/js/style.js
@@ -1,5 +1,11 @@
 let settingNav = document.querySelector(".setting-nav");
-
+let navItem = document.querySelectorAll(".nav-item, a[data-toggle='navitem']");
 let navSetting = function(){
 	settingNav.classList.toggle("active");
+}
+for (let i = 0; i < navItem.length; i++) {
+	navItem[i].addEventListener('click', function (e) {
+		let navbarDropdow = this.querySelector('.nav-bar-Dropdow');
+		navbarDropdow.classList.toggle('active');
+	});
 }
